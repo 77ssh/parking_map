1758d7f0d1f98270e0f6bef7e0f21dbd1850ff8f
송성하 <seongha7@seowoninfo.com>
2024-03-05 15:42:34 +0900

    화면에 지도 띄우기

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_naver_map/flutter_naver_map.dart';
+import 'package:parking_map/pages/home.dart';
 
-void main() {
+void main() async {
+  // 지도 초기화 하기
+  WidgetsFlutterBinding.ensureInitialized();
+  await NaverMapSdk.instance.initialize(clientId: 'm5t3bqnun7');
   runApp(const MyApp());
 }
 
@@ -16,7 +21,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
         useMaterial3: true,
       ),
-      //home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomePage(title: 'parking_map'),
     );
   }
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_naver_map/flutter_naver_map.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key, required this.title});
   final String title;
 
   @override
-  State<HomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(),
+    return Scaffold(
+      body: NaverMap(
+        options: const NaverMapViewOptions(),
+        onMapReady: (controller) {
+          debugPrint("네이버 맵 로딩 완료");
+        },
+      ),
     );
   }
 }
